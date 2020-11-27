d23e20484b1c15cf242072ec155274f2a1c22cbb
diego_neri <diego@diegoneri.com>
2020-11-27 19:56:32 -0300

    Finaliza o código fonte do projeto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Figgle;
+
+namespace VersaoOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeSO = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            string arquiteturaSO = System.Runtime.InteropServices.RuntimeInformation.OSArchitecture.ToString(); 
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Descubra qual a versão do seu Sistema Operacional");
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(FiggleFonts.Big.Render($"{nomeSO}"));
+            Console.WriteLine(FiggleFonts.Big.Render($"{arquiteturaSO}"));
+            Console.ResetColor();
+        }
+    }
+}
